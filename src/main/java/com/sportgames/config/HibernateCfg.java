1152a8478f5e1cec20ba06e561fe74330fdf9001
stan174 <johny31@yandex.ru>
2019-02-02 21:53:24 +0300

    тест 1

--- src/main/java/com/sportgames/config/HibernateCfg.java
+++ src/main/java/com/sportgames/config/HibernateCfg.java
@@ -7,8 +7,10 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+//@org.springframework.context.annotation.Configuration
 public class HibernateCfg {
-    public final SessionFactory sessionFactory;
+
+    private final SessionFactory sessionFactory;
 
     private HibernateCfg(){
         Configuration cfg=postgresCfg();
@@ -24,7 +26,7 @@ public class HibernateCfg {
         configuration.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
         configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/SportGames"); //5432
         configuration.setProperty("hibernate.connection.username", "root");
-        configuration.setProperty("hibernate.connection.password", "root");
+        configuration.setProperty("hibernate.connection.password", "admin");
         configuration.setProperty("hibernate.show_sql", "true");
         configuration.setProperty("hibernate.hbm2ddl.auto", "create-drop");
         return configuration;
