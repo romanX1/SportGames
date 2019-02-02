a4ed521b3c5f7ec78471295d2ff422c3521e024f
Boroda76 <samborod76@gmail.com>
2019-02-02 11:17:21 -0500

    postgres config with model and correct server settings

--- src/main/java/com/sportgames/config/HibernateCfg.java
+++ src/main/java/com/sportgames/config/HibernateCfg.java
@@ -22,9 +22,9 @@ public class HibernateCfg {
 
         configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
-        configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5433"); //5432
-        configuration.setProperty("hibernate.connection.username", "root");
-        configuration.setProperty("hibernate.connection.password", "sport");
+        configuration.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/postgres"); //5432
+        configuration.setProperty("hibernate.connection.username", "postgres");
+        configuration.setProperty("hibernate.connection.password", "postgres");
         configuration.setProperty("hibernate.show_sql", "false");
         configuration.setProperty("hibernate.hbm2ddl.auto", "create");
         return configuration;
