29427d9437da316de0dd015b86505dde549ad5dd
Nikolay <sopilov@list.ru>
2019-02-02 19:31:25 +0300

    добавлены комментарии к полям

--- src/main/java/com/sportgames/model/SportEvent.java
+++ src/main/java/com/sportgames/model/SportEvent.java
@@ -1,4 +1,43 @@
 package com.sportgames.model;
 
+import java.util.Set;
+
 public class SportEvent {
+
+    private long id;
+    private String sport;
+    private String timeEvent;
+    private Set<String> players;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public String getTimeEvent() {
+        return timeEvent;
+    }
+
+    public void setTimeEvent(String timeEvent) {
+        this.timeEvent = timeEvent;
+    }
+
+    public Set<String> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Set<String> players) {
+        this.players = players;
+    }
 }
