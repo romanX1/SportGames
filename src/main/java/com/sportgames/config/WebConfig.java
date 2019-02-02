e0ba50edcb9d1b16102d51530a7e2f7e78414f81
roman111 <rimpstin@gmail.com>
2019-02-02 18:09:10 +0700

    transform jsp

--- src/main/java/com/sportgames/config/WebConfig.java
+++ src/main/java/com/sportgames/config/WebConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("com.sportgames.config")
+@ComponentScan("com.sportgames")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
