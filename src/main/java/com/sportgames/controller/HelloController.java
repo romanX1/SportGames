273c07f9a28ea26f113d114b9408a543f6c0d997
stan174 <johny31@yandex.ru>
2019-02-03 17:55:22 +0300

    Merge branch 'master' of https://github.com/romanX1/SportGames
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/sportgames/config/HibernateCfg.java

--- src/main/java/com/sportgames/controller/HelloController.java
+++ src/main/java/com/sportgames/controller/HelloController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -31,17 +33,28 @@ public class HelloController {
 
     @GetMapping("/addGround")
     public String addGround(Model model){
-        model.addAttribute("Playground", new Playground ());
+        //model.addAttribute("Playground", new Playground ());
 
         return "addground";
     }
 
     @GetMapping("/playgrounds")
-    public String playGrounds(Model model){
-        playgroundService.add(new Playground("Test #1"));
-        model.addAttribute("grounds",playgroundService.getAll());
+    public ModelAndView playGrounds(){
+        return playGroundsByType("");
+    }
 
-        return "playgrounds";
+    @GetMapping("/playgrounds/{type}")
+    public ModelAndView playGroundsByType(@PathVariable String type){
+       List<Playground> playgrounds;
+        if (type.isEmpty()) {
+            playgrounds = playgroundService.getAll();
+        } else {
+            playgrounds = playgroundService.getPlaygroundsBySportType(type);
+        }
+        ModelAndView modelAndView = new ModelAndView("playgrounds");
+        modelAndView.addObject("grounds", playgrounds);
+
+        return modelAndView;
     }
 
 //    @RequestMapping(value = { "/groundList" }, method = RequestMethod.GET)
