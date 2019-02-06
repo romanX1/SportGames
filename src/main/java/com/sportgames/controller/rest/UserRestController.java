31e525f36af625b238c3a4cccd21eb135b3219b6
roman111 <rimpstin@gmail.com>
2019-02-06 16:12:03 +0700

    WEB + JS

--- src/main/java/com/sportgames/controller/rest/UserRestController.java
+++ src/main/java/com/sportgames/controller/rest/UserRestController.java
@@ -1,28 +1,27 @@
 package com.sportgames.controller.rest;
 
 import com.sportgames.model.User;
+import com.sportgames.service.SportEventService;
 import com.sportgames.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/users/")
 public class UserRestController {
 
     @Autowired
-    UserService userService;
+    SportEventService sportEventService;
 
-//    @RequestMapping(value = "/outdoorpanel", method = RequestMethod.GET)
-//    public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
-//        System.out.println("Updating User " + id);
-//
-//        User currentUser = userService.findById(id);
-//
-//        if (currentUser==null) {
-//            System.out.println("User with id " + id + " not found");
-//            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
-//        }
-//        return new ResponseEntity<User>(currentUser, HttpStatus.OK);
-//    }
+    @RequestMapping(value="/{user}", method = RequestMethod.GET)
+    public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
+
+        User currentUser = new User();
+        currentUser.setName("Vasya");
+
+        return new ResponseEntity<User>(currentUser, HttpStatus.OK);
+    }
 }
