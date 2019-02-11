082e4dd00b03e69c79c535da03604302c76a3689
stan174 <johny31@yandex.ru>
2019-02-11 20:37:50 +0300

    fixed start

--- src/main/java/com/sportgames/Application.java
+++ src/main/java/com/sportgames/Application.java
@@ -1,14 +1,8 @@
 package com.sportgames;
 
 
-import com.sportgames.config.AddSportsToBase;
-import com.sportgames.config.TestData;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.annotation.Order;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
