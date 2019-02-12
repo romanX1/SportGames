1dd42b2854d611ad93a62583c88b307ebc50321c
Boroda76 <samborod76@gmail.com>
2019-02-12 04:10:08 -0500

    booterbrod

--- src/main/java/com/sportgames/Application.java
+++ src/main/java/com/sportgames/Application.java
@@ -13,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
 @SpringBootApplication
+@ComponentScan("com.sportgames")
 public class Application {
 
     public static void main(String[] args) {
