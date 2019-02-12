a558d49d328529a0e6adfa479db0c2d6ff31492a
stan174 <johny31@yandex.ru>
2019-02-12 15:07:04 +0300

    jhjhg

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,12 +2,3 @@ $(document).ready(function () {
     $('.nav li [href="'+location.pathname+'"]').parent().addClass('active');
 });
 
-function TestLogin1(login){
-    if(/^[a-zA-Z1-9]+$/.test(login) === false)
-    {alert('В логине должны быть только латинские буквы'); return false;}
-    if(login.length < 4 || login.length > 20)
-    { alert('В логине должен быть от 4 до 20 символов'); return false;}
-    if(parseInt(login.substr(0, 1)))
-    {alert('Логине должен начинаться с буквы'); return false;}
-    return true;
-}
