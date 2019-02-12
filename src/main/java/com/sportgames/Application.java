d53dd62ede9472c1e3c8bbb17b2c86aa391c1dfe
Boroda76 <samborod76@gmail.com>
2019-02-12 18:55:28 -0500

    Points, ymaps and PG creation done!!1!11!1 Yeah, beach =p

--- src/main/java/com/sportgames/Application.java
+++ src/main/java/com/sportgames/Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan(
