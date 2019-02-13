b82719d92950d6f38159d2b1cdab87e9c6e699e7
roman111 <rimpstin@gmail.com>
2019-02-13 15:55:05 +0700

    done3

--- src/main/java/com/sportgames/Application.java
+++ src/main/java/com/sportgames/Application.java
@@ -27,13 +27,13 @@ public class Application {
     }
 
 //
-//    @Bean(initMethod = "initSports")
-//    public AddSportsToBase initAddSportsToBase(){
-//        return new AddSportsToBase();
-//    }
-////
-//    @Bean(initMethod = "initData")
-//    public TestData initTestData(){
-//        return new TestData();
-//    }
+    @Bean(initMethod = "initSports")
+    public AddSportsToBase initAddSportsToBase(){
+        return new AddSportsToBase();
+    }
+//
+    @Bean(initMethod = "initData")
+    public TestData initTestData(){
+        return new TestData();
+    }
 }
