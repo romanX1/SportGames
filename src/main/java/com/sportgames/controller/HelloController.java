39958906af6bcb21b7eb69d481d6fc5c9b22caea
stan174 <johny31@yandex.ru>
2019-02-13 14:04:13 +0300

    redirect Login

--- src/main/java/com/sportgames/controller/HelloController.java
+++ src/main/java/com/sportgames/controller/HelloController.java
@@ -45,7 +45,13 @@ public class HelloController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login() {
+    public String login(@RequestParam(value = "error", required = false) String error,
+                        @RequestParam(value = "logout", required = false) String logout,
+                        Model model){
+        model.addAttribute("error", error != null);
+        model.addAttribute("logout", logout != null);
+
+
         return "login";
     }
 
